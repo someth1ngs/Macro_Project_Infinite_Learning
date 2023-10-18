fdb2219d2f6989783570cc83f4db4200f9734d70
Adi Nugraha Kartiwa <17200335@bsi.ac.id>
2023-10-18 17:07:35 +0700

    dailies

---
+++ app/src/main/java/com/dicoding/macroproject/CreateDailies.kt
@@ -0,0 +1,11 @@
+package com.dicoding.macroproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateDailies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_dailies)
+    }
+}
